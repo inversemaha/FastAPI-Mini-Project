17aacd49946d5aedeb2902fd28205ca66d184739
inversemaha <mahajur77@gmail.com>
2025-11-09 09:32:13 +0600

    Update exception handiling

--- Inventory-Management-API/routes/category.py
+++ Inventory-Management-API/routes/category.py
@@ -3,12 +3,13 @@ from config.database import get_db
 from schemas.category import CategoryCreate, CategoryUpdate, CategoryResponse
 from models.category import Category as CategoryModel
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
 
-router = APIRouter(prefix="/categories", tags=["Categories"])
+router = APIRouter(prefix="/category", tags=["Categories"])
 
 @router.get("/", response_model=list[CategoryResponse])
-def get_all_category(db: Session = Depends(get_db)):
-    db_category = db.query(CategoryModel).all()
+def get_all_category(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    db_category = db.query(CategoryModel).offset(skip).limit(limit).all()
     if not db_category:
         raise HTTPException(status_code=404, detail="Categories not found")
     return db_category
@@ -23,11 +24,18 @@ def get_category_by_id(id: int, db: Session = Depends(get_db)):
 @router.post("/", response_model=CategoryResponse)
 def add_category(category: CategoryCreate, db: Session = Depends(get_db)):
     # Fetch category data from request body
-    db_category = CategoryModel(**category.model_dump())
-    db.add(db_category)
-    db.commit()
-    db.refresh(db_category)
-    return db_category
+    try:
+        db_category = CategoryModel(**category.model_dump())
+        db.add(db_category)
+        db.commit()
+        db.refresh(db_category)
+        return db_category
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(status_code=409, detail="Category already exists")
+    except Exception:
+        db.rollback()
+        raise HTTPException(status_code=500, detail="Server error")
 
 @router.put("/{id}", response_model=CategoryUpdate)
 def update_category(id: int, category: CategoryUpdate, db: Session = Depends(get_db)):
@@ -37,17 +45,25 @@ def update_category(id: int, category: CategoryUpdate, db: Session = Depends(get
         raise HTTPException(status_code=404, detail="Category not found")
     
     #Update category
-    db_category.name = category.name
-    db.commit()
-    db.refresh(db_category)
-    return db_category
+    try:
+        for key, value in category.model_dump().items():
+            setattr(db_category, key, value)
+        db.commit()
+        db.refresh(db_category)
+        return db_category
+    except Exception:
+        db.rollback()
+        raise HTTPException(status_code=500, detail="Failed to update category")
 
 @router.delete("/{id}")
 def delete_category(id:int, db: Session = Depends(get_db)):
     db_category = db.query(CategoryModel).filter(CategoryModel.id == id).first()
     if not db_category:
         raise HTTPException(status_code=404, detail="Category ot found")
-    db.delete(db_category)
-    db.commit()
-    db.refresh()
-    return {"message": "Category deleted successfully"}
+    try:
+        db.delete(db_category)
+        db.commit()
+        return {"message": "Category deleted successfully"}
+    except Exception:
+        db.rollback()
+        raise HTTPException(status_code=500, detail="Failed to delete category")
